fe7980c8162179de806e0af9e626c4e5bb114d5b
mahesh jangid <mesh.jangid@gmail.com>
2022-07-28 15:47:14 +0530

    changes

---
+++ backend/utils.js
@@ -0,0 +1,58 @@
+import jwt from "jsonwebtoken";
+
+export const generateToken = (user) => {
+  return jwt.sign(
+    {
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+      isSeller: user.isSeller,
+    },
+    process.env.JWT_SECRET,
+    {
+      expiresIn: "1h",
+    }
+  );
+};
+
+export const isAuth = (req, res, next) => {
+  const authorization = req.headers.authorization;
+  console.log(authorization);
+  if (authorization) {
+    const token = authorization.split(" ")[1]; // Bearer XXXXXX
+    console.log(token);
+    jwt.verify(token, process.env.JWT_SECRET, (err, decode) => {
+      if (err) {
+        res.status(401).send({ message: "Invalid Token" });
+      } else {
+        req.user = decode;
+        next();
+      }
+    });
+  } else {
+    res.status(401).send({ message: "No Token" });
+  }
+};
+export const isAdmin = (req, res, next) => {
+  if (req.user && req.user.isAdmin) {
+    next();
+  } else {
+    res.status(401).send({ message: "Invalid Admin Token" });
+  }
+};
+export const isSeller = (req, res, next) => {
+  if (req.user && req.user.isSeller) {
+    next();
+  } else {
+    res.status(401).send({ message: "Invalid Seller Token" });
+  }
+};
+export const isSellerOrAdmin = (req, res, next) => {
+  console.log(req.user);
+  if (req.user && (req.user.isSeller || req.user.isAdmin)) {
+    next();
+  } else {
+    res.status(401).send({ message: "Invalid Admin/Seller Token" });
+  }
+};
