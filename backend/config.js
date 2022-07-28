bcd391f948ca6ae5b7b34d8a6f406678b1f4b4aa
mahesh jangid <mesh.jangid@gmail.com>
2022-07-28 15:48:49 +0530

    changes

--- backend/config.js
+++ backend/config.js
@@ -4,11 +4,10 @@ dotenv.config();
 
 export default {
   PORT: process.env.PORT || 5000,
-  MONGODB_URL: process.env.MONGODB_URL || "mongodb://localhost/mern-project",
+  MONGODB_URL: process.env.MONGODB_URL,
   GOOGLE_API_key: process.env.GOOGLE_API_key,
   PAYPAL_CLIENT_ID: process.env.PAYPAL_CLIENT_ID,
   accessKeyId: process.env.accessKeyId,
   secretAccessKey: process.env.secretAccessKey,
-  MAILGUN_DOMAIN: process.env.MAILGUN_DOMAIN,
   JWT_SECTRET: process.env.JWT_SECTRET,
 };
